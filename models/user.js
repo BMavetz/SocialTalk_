4e53045604ed06219d5ac60a538b1aa20c8e4c18
Brady Mavetz <b.mavetz@outlook.com>
2022-05-16 22:43:14 -0500

    completed user model

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const {validEmail} = require("validator");
+
+// Schema to create user model
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validEmail, 'invalid email'],
+    },
+    thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "thought",
+    }
+    ],
+    friends:[
+    {
+        type:Schema.Types.ObjectId,
+        ref:"user",
+    }
+    ]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+userSchema.virtual("numFriends").get(function(){
+    return this.friends.length;
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
